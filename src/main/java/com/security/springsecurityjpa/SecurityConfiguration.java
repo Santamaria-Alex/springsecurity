79c1d754b953df137258613a83c0b9f289041025
Alex Santamaria <santamariaalex146@gmail.com>
2021-07-30 22:51:37 -0500

    friyay

---
+++ src/main/java/com/security/springsecurityjpa/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.security.springsecurityjpa;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfiguration extends WebSecurityConfiguration {
+
+}
