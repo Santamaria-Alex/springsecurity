570f2b597ef7bfada85ad818c6aea427ccafe27e
Alex Santamaria <santamariaalex146@gmail.com>
2021-08-01 23:28:18 -0500

    added configure override method

--- src/main/java/com/security/springsecurityjpa/SecurityConfiguration.java
+++ src/main/java/com/security/springsecurityjpa/SecurityConfiguration.java
@@ -1,7 +1,12 @@
 package com.security.springsecurityjpa;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfiguration extends WebSecurityConfiguration {
-
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 }
