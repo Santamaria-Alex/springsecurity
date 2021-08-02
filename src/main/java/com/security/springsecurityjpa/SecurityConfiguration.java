11b1f556081bdab61f741f9abc77564125b3a66b
Alex Santamaria <santamariaalex146@gmail.com>
2021-08-01 23:39:40 -0500

    added method userDetailService

--- src/main/java/com/security/springsecurityjpa/SecurityConfiguration.java
+++ src/main/java/com/security/springsecurityjpa/SecurityConfiguration.java
@@ -1,12 +1,29 @@
 package com.security.springsecurityjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(userDetailsService())
+    }
+
+    //this block sets up authorization for 3 API's, admin, user, and all ("/user" for example)
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
